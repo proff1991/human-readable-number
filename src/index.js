826e3de0e417fcca5f9ce09e4f1056e23c5f0a12
proff1991 <parkhomenko.sitel@gmail.com>
2025-07-26 18:37:58 +0300

    feat: fix the problems

--- src/index.js
+++ src/index.js
@@ -1,32 +1,63 @@
 module.exports = function toReadable(number) {
-  var first = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'sevene', 'eight', 'nine'];
-  var secondBefore20 = ['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
-  var secondAfter20 = ['',
+  const first = [
     '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  const secondBefore20 = [
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const secondAfter20 = [
+    '',
+    '',
+    'twenty',
     'thirty',
     'forty',
     'fifty',
     'sixty',
     'seventy',
     'eighty',
-    'ninety'
+    'ninety',
   ];
 
-  return number === 0 ? 'zero' : String(number).split('').reverse().map((char,index,array)=> {
-
-    if(index === 0 && array[1] !== '1'){
-      return first[char];
-    }else if(index === 0 && array[1] === '1'){
-      return '';
-    }else if(index === 1 && char === '1'){
-      return secondBefore20[char];
-    }else if(index === 1){
-      return secondAfter20[char];
-    }else if(index === 2){
-      return `${first[char]} hundred`;
-    }
-
-  }).reverse().join('');
-
-  throw new Error('Not implemented');
+  return number === 0
+    ? 'zero'
+    : String(number)
+        .split('')
+        .reverse()
+        .map((char, index, array) => {
+          if (index === 0 && array[1] !== '1') {
+            return first[char];
+          }
+          if (index === 0 && array[1] === '1') {
+            return '';
+          }
+          if (index === 1 && char === '1') {
+            return secondBefore20[array[0]];
+          }
+          if (index === 1) {
+            return secondAfter20[char];
+          }
+          return `${first[char]} hundred`;
+        })
+        .reverse()
+        .join(' ')
+        .replace(/\s+/g, ' ')
+        .trim();
 };
