1bad2b52c61d2531cc512fdfcc8b30c6c7130652
proff1991 <parkhomenko.sitel@gmail.com>
2025-07-26 18:28:42 +0300

    feat: first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-module.exports = function toReadable(/* number */) {
+module.exports = function toReadable(number) {
+  var first = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'sevene', 'eight', 'nine'];
+  var secondBefore20 = ['', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen',];
+  var secondAfter20 = ['',
+    '',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety'
+  ];
+
+  return number === 0 ? 'zero' : String(number).split('').reverse().map((char,index,array)=> {
+
+    if(index === 0 && array[1] !== '1'){
+      return first[char];
+    }else if(index === 0 && array[1] === '1'){
+      return '';
+    }else if(index === 1 && char === '1'){
+      return secondBefore20[char];
+    }else if(index === 1){
+      return secondAfter20[char];
+    }else if(index === 2){
+      return `${first[char]} hundred`;
+    }
+
+  }).reverse().join('');
+
   throw new Error('Not implemented');
 };
